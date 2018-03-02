3ce0764d6ce578914eb84756256f82ffa65045ca
Bruce Bookman <bruce.bookman@gmail.com>
2018-03-02 15:44:17 -0800

    Initial Commit

---
+++ LocalWeen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalWeen
+//
+//  Created by Bruce Bookman on 3/2/18.
+//  Copyright © 2018 Bruce Bookman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
