5fa2cf4ef6f73bdcc449f188a68ee958544f347c
Bruce Bookman <bruce.bookman@gmail.com>
2018-03-03 14:36:16 -0800

    adding ui and connections to controller

--- LocalWeen/Controller/LocationDetialViewController.swift
+++ LocalWeen/Controller/LocationDetialViewController.swift
@@ -13,6 +13,28 @@ import GoogleSignIn
 
 
 class LocationDetialViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate {
+    
     public var coordinate:CLLocationCoordinate2D? = CLLocationCoordinate2D()
+    private let dbHandler = DBHandler()
+    private let locationManager = CLLocationManager()
+    private var picker:UIImagePickerController?=UIImagePickerController()
+    
+    //Outlets
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var cosmosView: CosmosView!
+    @IBOutlet weak var userChosenPhotoFromGalleryOrCamera: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let coordinate = locationManager.location?.coordinate else {
+            print("Can't get user location")
+            return
+        }
+    }
+    
+    
 
 }
