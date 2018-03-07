71e4f58f99a59d3edd1cd444674db2a8081fed9d
Bruce Bookman <bruce.bookman@gmail.com>
2018-03-06 22:02:26 -0800

    massive changes made for refactor

--- LocalWeen/Controller/LocationDetialViewController.swift
+++ LocalWeen/Controller/LocationDetialViewController.swift
@@ -37,7 +37,7 @@ class LocationDetialViewController: UIViewController, UIImagePickerControllerDel
         getLocationPhotos(coordinate: coord!)
         if cosmosView.rating <= 0  {
             addButton.isEnabled = false
-        }
+        }//if
         cosmosView.didFinishTouchingCosmos = didFinishTouchingCosmos
         cosmosView.didTouchCosmos = didTouchCosmos
         picker?.delegate = self
@@ -45,7 +45,7 @@ class LocationDetialViewController: UIViewController, UIImagePickerControllerDel
         usrGivenName.text = social.usrGivenName
         usrProfilePhoto.image = social.usrProfilePhoto
         averageRating(coordinate: coord!)
-    }
+    }//viewDidLoad
     
     private func reverseGeocodeCoordinate(_ coordinate: CLLocationCoordinate2D) {
         let geocoder = GMSGeocoder()
@@ -61,7 +61,7 @@ class LocationDetialViewController: UIViewController, UIImagePickerControllerDel
     @IBAction func addButton(_ sender: UIButton) {
         guard let coordinate = self.coord else {
             fatalError("Can't get coordinate")
-        }
+        }//guard
         // store location rating and possibly image path if an image was chosen
         if userChosenPhotoFromGalleryOrCamera.image != nil {
             let imageName:String = storageHandler.upLoad(imageToUpload: userChosenPhotoFromGalleryOrCamera.image!)
@@ -69,35 +69,33 @@ class LocationDetialViewController: UIViewController, UIImagePickerControllerDel
         } else {
             //don't upload an image, just save the location rating and coord
             dbHandler.addLocation(coordinate: coordinate, rating: cosmosView.rating, imageName: "")
-        }
-        
-    }
+        }//else
+    }//addButton
     
     //MARK Cosmos Ratings
     private func didTouchCosmos(_ rating: Double){
         addButton.isEnabled = true
-    }
+    }//didTouchCosmos
     
     private func didFinishTouchingCosmos(_ rating: Double){
         addButton.isEnabled = true
-    }
+    }//didFinishTouchingCosmos
     
     //MARK: Choose Photo
     @IBAction func addPhoto(_ sender: Any) {
         openGallary()
-    }
+    }//addPhoto
     
     @IBAction func addPhotoFromCamera(_ sender: Any) {
         openCamera()
-    }
-    
+    }//addPhotoFromCamera
     
     private func openGallary()
     {
         picker!.allowsEditing = false
         picker!.sourceType = UIImagePickerControllerSourceType.photoLibrary
         present(picker!, animated: true, completion: nil)
-    }
+    }//openGallary
     
      func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
@@ -105,7 +103,7 @@ class LocationDetialViewController: UIViewController, UIImagePickerControllerDel
         
         dismiss(animated: true, completion: nil)
         userChosenPhotoFromGalleryOrCamera.isHidden = false
-    }
+    }//imagePickerController
     
     private func openCamera() {
         if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)){
@@ -113,44 +111,40 @@ class LocationDetialViewController: UIViewController, UIImagePickerControllerDel
             picker!.sourceType = UIImagePickerControllerSourceType.camera
             picker!.cameraCaptureMode = .photo
             present(picker!, animated: true, completion: nil)
-        }else{
+        } else {
             let alert = UIAlertController(title: "Camera Not Found", message: "This device has no Camera", preferredStyle: .alert)
             let ok = UIAlertAction(title: "OK", style:.default, handler: nil)
             alert.addAction(ok)
             present(alert, animated: true, completion: nil)
-        }
+        }//else
     }//openCamera
     
     //MARK: Get Location Photos
     func getLocationPhotos(coordinate:CLLocationCoordinate2D){
-        dbHandler.getMatching(coordinate: coordinate) { (photoNames) in
-            for photo in photoNames {
-                //grab photo and stick it in the UI
-                
+        dbHandler.getFor(coordinateIn: coordinate, what: "fileNames") { (fileNames) in
+            for file in fileNames{
+                //Display the photos
             }//for
-        }//getMatching
+        }//dbHandler
     }//getLocationPhotos
     
     func averageRating(coordinate:CLLocationCoordinate2D){
         var totalRating:Double = 0
-        dbHandler.getRatings(coordinate: coordinate) { (ratings) in
+        dbHandler.getFor(coordinateIn: coordinate, what: "ratings") { (ratings) in
             for rating in ratings {
-                
-                totalRating += rating as Double
-                
-            }
+                totalRating += rating as! Double
+            }//for ratings
+            
             var av:Double = 0
             av = (Double(totalRating))/(Double(ratings.count))
             let avRatingStr = String(format: "%.2f", ceil(av * 100)/100)
             self.averageRatingLabel.text = "Average: " + avRatingStr
-
-        }
-    }
+        }//dbHandler
+    }//averageRating
     
     @IBAction func Back(_ sender: UIButton) {
         performSegue(withIdentifier: "toMap", sender: self)
-        
-    }
+    }//Back
     
     
 }//LocationDetailViewController
--- LocalWeen/Service/Database.swift
+++ LocalWeen/Service/Database.swift
@@ -12,6 +12,78 @@ import CoreLocation
 class DBHandler{
     var ref:DatabaseReference! = Database.database().reference().child("locations")
     
+    
+    func getFor(coordinateIn:CLLocationCoordinate2D?, what: String, completion: @escaping ([Any]) -> ())  {
+        
+        var ratings = [Double]()
+        var fileNames = [String]()
+        
+        ref.observeSingleEvent(of: .value) { (snapshot) in
+            if let snapshot =  snapshot.children.allObjects as? [DataSnapshot]{
+                for snap in snapshot {
+                    if let data = snap.value as? [String:Any]{
+                        //Get the lattitude and longitude for matching
+                        guard let lattitude = data["lattitude"] else {
+                            return
+                        }
+                        guard let longitude = data["longitude"] else {
+                            return
+                            
+                        }
+                        let coordFromDB = CLLocationCoordinate2DMake(lattitude as! CLLocationDegrees, longitude as! CLLocationDegrees)
+                        
+                        let isMatch = self.matchCoords(coordinateIn: coordinateIn!, coordFromDB: coordFromDB)
+                        
+                        switch what {
+                            
+                            case "fileNames":
+                                guard let filename = data["image_name"] else {
+                                    return
+                                }
+                                if isMatch {
+                                        fileNames.append(filename as! String)
+                                }//if isMatch
+                            
+                            case "ratings":
+                            
+                                guard let ratingData = data["rating"] else {
+                                    return
+                                }
+                                
+                                if isMatch {
+                                    
+                                    let rating = ratingData as! Double
+                                    if rating >= 1.0 {
+                                        ratings.append(rating)
+                                    } else {
+                                        print("Rating was less than 1")
+                                    }
+                                    ratings.append(rating)
+                                }//isMatch
+                            
+                            case "coordinate":
+                                var coordinates = [CLLocationCoordinate2D]()
+                                coordinates.append(coordFromDB)
+
+                        default:
+                            return
+                        }//switch
+                    }//data
+                }//for
+            }//snapshot
+        }//ref
+    }//get
+    
+    private func matchCoords(coordinateIn:CLLocationCoordinate2D, coordFromDB:CLLocationCoordinate2D) -> Bool {
+        
+        if coordinateIn.latitude == coordFromDB.latitude && coordinateIn.longitude == coordFromDB.longitude {
+            return true
+        } else {
+            return false
+        }
+    }//matchCoords
+
+    
     func addLocation(coordinate:CLLocationCoordinate2D, rating: Double, imageName: String?){
         let location = ["latitude": coordinate.latitude,
                         "longitude": coordinate.longitude,
@@ -23,55 +95,7 @@ class DBHandler{
     }//end setLocation
     
     
-    //open func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Swift.Void) -> URLSessionDataTask
+}//DBHandler
     
-    func getMatching(coordinate:CLLocationCoordinate2D, completion: @escaping ([String?]) -> ())  {
-        var images = [String]()
-        
-        let ref:DatabaseReference = Database.database().reference().child("locations")
-        ref.observeSingleEvent(of: .value) { (snapshot) in
-            if let snapshot =  snapshot.children.allObjects as? [DataSnapshot]{
-                for snap in snapshot {
-                    if let data = snap.value as? [String:Any]{
-                        let imageName = data["image_name"]
 
-                        images.append(imageName! as! String)
-                    }//if
-                    else{
-                        fatalError("Can't get a Firebase snapshot")
-                    }
-                }//for
-            }//if let
-            completion(images)
-        }//ref
-    }//getMatching
     
-    func getRatings(coordinate:CLLocationCoordinate2D, completionHandler: @escaping ([Double]) -> ()) {
-        ref.observeSingleEvent(of: .value) { (snapshot) in
-            var ratings = [Double]()
-            print("in getRatings")
-            if let snapshot =  snapshot.children.allObjects as? [DataSnapshot]{
-                print("in let snapshot")
-                for snap in snapshot {
-                    print("in snap")
-                    if let data = snap.value as? [String:Any]{
-                        print("in let data")
-                        guard let ratingData = data["rating"] else {
-                            print("No rating found")
-                            return
-                        }
-                        let rating = ratingData as! Double
-                        if rating >= 1.0 {
-                            ratings.append(rating)
-                        } else {
-                            print("Rating was less than 1")
-                        }
-                    }//if
-                }//for
-            }//if
-            completionHandler(ratings)
-        }//ref
-    }
-
-}
-
